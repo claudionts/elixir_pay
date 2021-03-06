ade0afcf873c2730575f55187ef426de02fc659f
claudionts <claudionts@gmail.com>
2021-03-06 08:31:08 -0300

    Insert user with account

--- lib/elixir_pay/users/create.ex
+++ lib/elixir_pay/users/create.ex
@@ -1,10 +1,29 @@
 defmodule ElixirPay.Users.Create do
-  alias ElixirPay.{Repo, User}
+  alias Ecto.Multi
+  alias ElixirPay.{Account, Repo, User}
 
   def call(params) do
-    
-    # params
-    # |> User.changeset
-    # |> Repo.insert
+    Multi.new
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      insert_account(repo, user)
+    end)
+    |> run_transaction()
+
+  end
+
+  defp insert_account(repo, user) do
+    user.id
+    |> account_changeset
+    |> repo.insert
+  end
+
+  defp account_changeset(user_id), do: Account.changeset(%{user_id: user_id, balance: "0.00"})
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{create_account: account}} -> IO.inspect(account)
+    end
   end
 end
