708609ca00907b239a5c3a4612d4f9bd2ceb1f98
claudionts <claudionts@gmail.com>
2021-03-06 16:22:19 -0300

    get account join user

--- lib/elixir_pay/users/create.ex
+++ lib/elixir_pay/users/create.ex
@@ -8,6 +8,9 @@ defmodule ElixirPay.Users.Create do
     |> Multi.run(:create_account, fn repo, %{create_user: user} ->
       insert_account(repo, user)
     end)
+    |> Multi.run(:preload_data, fn repo, %{create_user: user} ->
+      preload_data(repo, user)
+    end)
     |> run_transaction()
 
   end
@@ -20,10 +23,14 @@ defmodule ElixirPay.Users.Create do
 
   defp account_changeset(user_id), do: Account.changeset(%{user_id: user_id, balance: "0.00"})
 
+  defp preload_data(repo, user) do
+    {:ok, repo.preload(user, :account)}
+  end
+
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, %{create_account: account}} -> IO.inspect(account)
+      {:ok, %{preload_data: user}} -> {:ok, user}
     end
   end
 end
