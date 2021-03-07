a7cdc4f5e48bf287b9e1d68468e1c5632a8212d4
claudionts <claudionts@gmail.com>
2021-03-07 12:48:13 -0300

    Create transaction route

--- lib/elixir_pay.ex
+++ lib/elixir_pay.ex
@@ -1,11 +1,13 @@
 defmodule ElixirPay do
   alias ElixirPay.Users.Create, as: UserCreate
 
-  alias ElixirPay.Accounts.{Deposit, Withdraw}
+  alias ElixirPay.Accounts.{Deposit, Transaction, Withdraw}
   
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit(params), to: Deposit, as: :call
 
   defdelegate withdraw(params), to: Withdraw, as: :call
+
+  defdelegate transaction(params), to: Transaction, as: :call
 end
--- lib/elixir_pay_web/controllers/accounts_controller.ex
+++ lib/elixir_pay_web/controllers/accounts_controller.ex
@@ -20,4 +20,12 @@ defmodule ElixirPayWeb.AccountsController do
       |> render("update.json", account: account)
     end
   end
+
+  def transaction(conn, params) do
+    with {:ok, %{} = transaction} <- ElixirPay.transaction(params) do
+      conn
+      |> put_status(:created)
+      |> render("transaction.json", transaction: transaction)
+    end
+  end
 end
